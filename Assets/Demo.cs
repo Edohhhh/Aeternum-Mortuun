026f06e9ddba036e151e84fe4b1e64b3aa9ee9dd
Edohhhh <elric9959@gmail.com>
2025-04-23 19:56:07 -0300

    0.0.3
    -Agregado de dotween
    -Ruleta de prueba para estadisticas de player
    -Heredar estadisticas desde la ruleta.

---
+++ Assets/Demo.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using EasyUI.PickerWheelUI;
+using UnityEngine.UI;
+using TMPro;
+
+public class Demo : MonoBehaviour
+{
+    [SerializeField] private Button uiSpinButton;
+    [SerializeField] private TextMeshProUGUI uiSpinButtonText;
+    [SerializeField] private PickerWheel pickerWheel;
+
+    private void Start()
+    {
+        uiSpinButton.onClick.AddListener(() =>
+        {
+            uiSpinButton.interactable = false;
+            uiSpinButtonText.text = "Spinning";
+
+            pickerWheel.OnSpinStart(() =>
+            {
+                Debug.Log("Spin Started...");
+            });
+
+            pickerWheel.OnSpinEnd(wheelPiece =>
+            {
+                Debug.Log("Pieza seleccionada: " + wheelPiece.Label);
+
+                switch (wheelPiece.Index)
+                {
+                    case 0:
+                        Debug.Log("Resultado: Salud aumentada en 20 puntos");
+                        break;
+
+                    case 1:
+                        Debug.Log("Resultado: Daño de bala aumentado en 3 puntos");
+                        break;
+
+                    case 2:
+                        Debug.Log("Resultado: Cuchillo activado).");
+                        break;
+
+                    case 3:
+                        Debug.Log("Resultado: Torreta activada.");
+                        break;
+
+                    case 4:
+                        Debug.Log("Resultado: Bala extra añadida al pool.");
+                        break;
+
+                    case 5:
+                        Debug.Log("Resultado: Lanzallamas activado.");
+                        break;
+
+                    case 6:
+                        Debug.Log("Resultado: Escudo reflectante activado.");
+                        break;
+
+                    case 7:
+                        Debug.Log("Resultado: Mina activada.");
+                        break;
+
+                    default:
+                        Debug.Log("Resultado: No se asignó ningún efecto a esta pieza.");
+                        break;
+                }
+
+                uiSpinButton.interactable = true;
+                uiSpinButtonText.text = "Spin";
+            });
+
+            pickerWheel.Spin();
+        });
+    }
+}
+
