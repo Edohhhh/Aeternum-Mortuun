cf28b1c1bc7e191066ba4316c0dedf47ea6dd4d7
Valadinho <corchito107@gmail.com>
2025-10-01 18:56:06 -0300

    0.5.1

--- Assets/Scripts/PowerUps/PestTrail/AcidCloud.cs
+++ Assets/Scripts/PowerUps/PestTrail/AcidCloud.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class AcidCloud : MonoBehaviour
 {
-    public int damage = 5;
+    public int damagePerTick = 1;
+    public float tickInterval = 1f;
     public float lifetime = 3f;
+
+    private HashSet<EnemyHealth> enemiesInZone = new();
 
     private void Start()
     {
         Destroy(gameObject, lifetime);
+        StartCoroutine(DamageOverTime());
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -17,7 +23,32 @@ public class AcidCloud : MonoBehaviour
         var enemy = other.GetComponent<EnemyHealth>();
         if (enemy != null)
         {
-            enemy.TakeDamage(damage, Vector2.zero, 0f);
+            enemiesInZone.Add(enemy);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy")) return;
+
+        var enemy = other.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemiesInZone.Remove(enemy);
+        }
+    }
+
+    private IEnumerator DamageOverTime()
+    {
+        while (true)
+        {
+            foreach (var enemy in enemiesInZone)
+            {
+                if (enemy != null)
+                    enemy.TakeDamage(damagePerTick, transform.position, 0f);
+            }
+
+            yield return new WaitForSeconds(tickInterval);
         }
     }
 }
--- Assets/Scripts/Screens/Buttons.cs
+++ Assets/Scripts/Screens/Buttons.cs
@@ -6,7 +6,7 @@ public class MenuButtons : MonoBehaviour
 
     public void PlayAgain()
     {
-        SceneManager.LoadScene("BossSlime");
+        SceneManager.LoadScene("LoopSlime1");
     }
 
     public void Menu()
